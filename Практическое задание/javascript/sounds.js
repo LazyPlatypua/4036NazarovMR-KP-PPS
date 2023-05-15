f014263b12ae5148596c13c9ebb6d4831512d886
Mikhail <nazarovmisha@yandex-team.ru>
2023-05-15 11:40:05 +0300

    Чуть правок

--- Практическое задание/javascript/sounds.js
+++ Практическое задание/javascript/sounds.js
@@ -1,22 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var snd = new Audio('media/vine-boom.mp3');
-    document.getElementById('username-input').oninput = function() {
-        snd.currentTime = 0;
-        snd.play();
-    };
     document.getElementById('email').oninput = function() {
         snd.currentTime = 0;
         snd.play();
     };
-    document.getElementById('phone').oninput = function() {
-        snd.currentTime = 0;
-        snd.play();
-    };
     document.getElementById('confirm_password').oninput = function() {
-        snd.currentTime = 0;
-        snd.play();
-    };
-    document.getElementById('user_class').oninput = function() {
         snd.currentTime = 0;
         snd.play();
     };
