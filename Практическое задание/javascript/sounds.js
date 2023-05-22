95c8280066e48b898854b481a06c4beed3650da8
Mikhail <nazarovmisha@yandex-team.ru>
2023-05-22 20:11:55 +0300

    Замена второй кнопки отправить
    Заменил вторую кнопку отправить? Теперь вместо того, чтобы она блокировала страницу, она воспроизводит звук

--- Практическое задание/javascript/sounds.js
+++ Практическое задание/javascript/sounds.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var snd = new Audio('media/vine-boom.mp3');
+    var audio = new Audio("media/nope.mp3");
     document.getElementById('email').oninput = function() {
         snd.currentTime = 0;
         snd.play();
@@ -8,4 +9,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         snd.currentTime = 0;
         snd.play();
     };
+
+    var button = document.getElementById("sound_button");
+
+    button.addEventListener("click", function() {
+    audio.play();
+  });
 })
